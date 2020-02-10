98bce01a3c8b35110eb1cec4926a8c596a4af58f
harry-chiu <harrychiu1106@gmail.com>
2020-02-10 22:51:38 +0800

    Updates

---
+++ precache-manifest.2948f327d6f815f4fe00cf35b24e0b47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ed9307b0475bfb09c8b642bfd61855",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "7ada2f23889141d93489",
+    "url": "/react-redux-todo/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "849e67a93e5311e6aa89",
+    "url": "/react-redux-todo/static/css/main.dafd4787.chunk.css"
+  },
+  {
+    "revision": "7ada2f23889141d93489",
+    "url": "/react-redux-todo/static/js/2.2724e15c.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-redux-todo/static/js/2.2724e15c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "849e67a93e5311e6aa89",
+    "url": "/react-redux-todo/static/js/main.9cf0d262.chunk.js"
+  },
+  {
+    "revision": "7c7024619c201c9627e8",
+    "url": "/react-redux-todo/static/js/runtime-main.b42a28ab.js"
+  }
+]);
